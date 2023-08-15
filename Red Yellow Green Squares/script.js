602e42e2dd2d71bb6a22318cf05b15d2eff19356
YashSingh08 <yashsingh9557090791@gmail.com>
2023-08-15 10:25:37 +0530

    frist commit Uploading all projects till now

---
+++ Red Yellow Green Squares/script.js
@@ -0,0 +1,26 @@
+let titleDiv = document.getElementById('title')
+
+titleDiv.style.backgroundColor = 'blue'
+
+const squares = document.querySelectorAll('.colorSquare')
+
+// forEach
+const timesClicked = {'red': 0, 'yellow': 0, 'green': 0}
+squares.forEach(square => {
+  square.onclick = () => {
+    timesClicked[square.value] += 1
+    square.innerText = timesClicked[square.value]
+  }
+})
+function clearScores() {
+  timesClicked.red = 0
+  timesClicked.yellow = 0
+  timesClicked.green = 0
+  squares.forEach(square => {
+    square.innerText = ''
+  })
+}
+
+const clearGameBtn = document.getElementById('clear-game')
+clearGameBtn.onclick = () => clearScores()
+
